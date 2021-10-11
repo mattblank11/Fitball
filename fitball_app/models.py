f66e6095692110914d6fb7b179bead5261e5e7bc
MATT BLANK <mattblank@MattBook.local>
2021-10-11 09:41:04 -0400

    New model: Competition
    Summary:
    - Stores all the data related to competitions

--- fitball_app/models.py
+++ fitball_app/models.py
@@ -15,12 +15,12 @@ class Connected_Device(models.Model):
     device_user_id = models.CharField(max_length = 200)
     device_access_token = models.CharField(max_length = 200)
     device = models.ForeignKey(Device, on_delete = models.CASCADE, null = True)
-    user = models.ForeignKey(User, default = None, null = True, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, default = None, blank = True, null = True, on_delete = models.CASCADE)
     def __str__(self):
         return f'{self.user.username} // {self.device.device_name}'
 
 class Goal(models.Model):
-    user = models.ForeignKey(User, default = None, null = True, on_delete = models.CASCADE)
+    user = models.ForeignKey(User, default = None, blank = True, null = True, on_delete = models.CASCADE)
     device = models.ForeignKey(Device, on_delete = models.CASCADE, null = True)
     goal_category = models.CharField(max_length = 200)
     clean_goal_metric = models.CharField(max_length = 200)
@@ -28,8 +28,8 @@ class Goal(models.Model):
     goal_value = models.FloatField(max_length = 200)
     goal_dollars = models.FloatField(max_length = 200)
     active = models.BooleanField(default = False)
-    start_date = models.DateTimeField(null = True)
-    end_date = models.DateTimeField(null = True)
+    start_date = models.DateTimeField(blank = True, null = True)
+    end_date = models.DateTimeField(blank = True, null = True)
     def __str__(self):
         return f'{self.user.username} // {self.goal_category} // {self.clean_goal_metric} // {self.goal_value}'
 
@@ -38,3 +38,18 @@ class Discord(models.Model):
     discord_id = models.CharField(max_length = 200)
     def __str__(self):
         return self.discord_id
+
+class Competition(models.Model):
+    name = models.CharField(max_length = 200)
+    metric = models.CharField(max_length = 200)
+    format = models.CharField(max_length = 200)
+    goal_value = models.FloatField(default = 0)
+    dollars = models.FloatField(default = 0)
+    active = models.BooleanField(default = True)
+    private = models.BooleanField(default = True)
+    start_date = models.DateTimeField(blank = True, null = True)
+    end_date = models.DateTimeField(blank = True, null = True)
+    users = models.ManyToManyField(User, blank = True)
+    device = models.ForeignKey(Device, on_delete = models.CASCADE, blank = True, null = True)
+    def __str__(self):
+        return f'{self.name} // {self.device} // {self.metric} // {self.format}'
